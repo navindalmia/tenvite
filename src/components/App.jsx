2198f050411360fb6180d6c79d3dcf403d4b5bae
navindalmia <navindalmia2003@yahoo.co.in>
2024-04-08 18:19:30 +0100

    added user core and best score with local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,40 @@ import viteLogo from '/vite.svg'
 import '../style/App.css'
 import Die from './Die.jsx'
 import { nanoid } from "nanoid"
+import ReactConfetti from 'react-confetti'
+//import useLS from './useLS.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
   const [dice, setDice] = useState(allNewDice()  )
   const [tenzies,setTenzies] =useState(false)
+  const [userScore,setUserScore]=useState(0)
 
+ // const [hiScore,setHiScore] =useLS("hiScore",0)
+ const [hiScore, setHiScore] = useState(() => {
+  // getting stored value
+  const saved = localStorage.getItem("hiScore");
+  const initialValue = JSON.parse(saved);
+  console.log("initial hiscore"+initialValue)
+  if (initialValue ==null){
+console.log("initialValue is null")
 
+localStorage.setItem("hiScore", JSON.stringify(999));
 
+  }
+  return initialValue || 999;
+});
+
+ useEffect(() => {
+  // storing input name
+  console.log('hiscore')
+  const saved = localStorage.getItem("hiScore");
+  console.log('hi:'+saved)
+  if ( saved>userScore &&  userScore!=0){
+  localStorage.setItem("hiScore", JSON.stringify(userScore));
+  setHiScore(userScore)
+  }
+}, [tenzies]);
 
 
 
@@ -61,8 +87,10 @@ function App() {
     if(tenzies){
       setTenzies(false)
       setDice(allNewDice())
+      setUserScore(0)
     }
     else{
+      setUserScore(prev=>prev+1)
     setDice(prevDice => prevDice.map(die => {
 
       return die.held === true ? die :
@@ -76,11 +104,16 @@ function App() {
   }
   }
 
-
+ 
   useEffect(()=>{console.log("Winner!")
   let result = dice.every(checkTenzies)
-  let tenzie = result=== true? "Well Done, you WIN!":null
-  tenzie && setTenzies(true)
+  // let tenzie = result=== true? "Well Done, you WIN!":null
+  let tenzie = result=== true? true:null
+ if( tenzie){
+   
+  setTenzies(true)
+  // <ReactConfetti />
+ }
 }, [dice])
   
 
@@ -97,19 +130,38 @@ function checkTenzies(el,index,arr){
 
 
 }
+
+function clearHiScore(){
+  localStorage.removeItem("hiScore")
+
+}
+
+
   return (
     <>
       <main>
+     { tenzies && <ReactConfetti/>}
       <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             
-        <div className="dice-container">
-          {diceElements}
-  
+         <div className="central">
+           <div className="your-score" >Your Score</div>
+           <br/>
+           {userScore}
+           {/* <div className="your-score-value" >10</div> */}
+           
+          <div className="dice-container">
+            {diceElements}
+    
 
 
-        </div>
+          </div>
+          <div className="hi-score" >Best Score</div>
+          <br/>
+           {hiScore}
+        </div>   
         <button onClick={rollDice} className="rollDice">{tenzies? "New Game": "Roll" }</button>
+        <button onClick={clearHiScore} className="rollDice">Clear Best Score</button>
 
       </main>
 
